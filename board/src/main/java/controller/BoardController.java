94c6622486dcd3ba3860de3b3546a69ae4c3a1fa
github dongjin1996 <github ehwl01@naver.com>
2023-06-08 18:20:59 +0900

    6/8

--- board/src/main/java/controller/BoardController.java
+++ board/src/main/java/controller/BoardController.java
@@ -1,22 +1,28 @@
 package controller;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.beanutils.BeanUtils;
 
 import DAO.BoardDAO;
 import DTO.Board;
 
 
 @WebServlet("/")
+@MultipartConfig(maxFileSize=1024*1024*2, location="c:/Temp/img")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private BoardDAO dao; //model
@@ -44,10 +50,34 @@ public class BoardController extends HttpServlet {
 		
 		//1.경로를 정해준다(라우팅)
 		switch (command) {
-			case "/list" : site = getList(request); break;
+		case "/list" : site = getList(request); break;
+		case "/view" : site = getView(request); break;
+		case "/write" : site = "write.jsp"; break; //글쓰기 화면을 보여줌
+		case "/insert" : site = insertBoard(request); break; //글등록
+		case "/edit" : site = getViewForEdit(request); break; //수정 화면 보여줌
+		case "/update" : site = updateBoard(request); break; // 글수정
+		case "/delete" : site = deleteBoard(request); break; //글 삭제하기
 		}
 		
-		ctx.getRequestDispatcher("/" + site).forward(request, response);
+		/*
+		  -둘다 새로운 페이지로 이동
+		  redirect: 데이터(response, request 객체) 를 가지고 이동하지 X, 주소가 변한다.
+		  *DB에 변화가 생기는 요청(글쓰기, 회원가입...) 
+		  *insert, update, delete..
+		  
+		  forward: 데이터(response, request 객체) 를 가지고 이동 O, 주소가 변하지않는다
+		   단순조회(상세페이지 보기, 리스트 보기, 검색..)
+		   *select... 		  
+		  */
+		
+		if(site.startsWith("redirect:/")) { //redirect
+			String rview = site.substring("redirect:/".length());
+			System.out.println("rview: " + rview);
+			
+			response.sendRedirect(rview); //페이지 이동 
+		}else { //forward 
+			ctx.getRequestDispatcher("/" + site).forward(request, response);			
+		}
 	}
 	
 	public String getList(HttpServletRequest request) {
@@ -62,4 +92,121 @@ public class BoardController extends HttpServlet {
 		
 		return "index.jsp";
 	}
+	
+	public String getView(HttpServletRequest request) {
+		int board_no = Integer.parseInt(request.getParameter("board_no"));
+		
+		try {
+			dao.updateViews(board_no);
+			Board b = dao.getView(board_no);
+			request.setAttribute("board", b);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return "view.jsp";
+	}
+	
+	public String insertBoard(HttpServletRequest request) {
+		Board b = new Board();
+		
+		try {
+			BeanUtils.populate(b, request.getParameterMap());
+			
+			//1. 이미지 파일 자체를 서버 컴퓨터에 저장
+			Part part = request.getPart("file"); //이미지파일 받기
+			String fileName = getFilename(part); //파일 이름 구하기
+			
+			if(fileName != null && !fileName.isEmpty()) { //fileName이 null이 아니고 빈값이 아니라면
+				part.write(fileName); //파일을 컴퓨터에 저장한다.
+			}
+			
+			//2. 이미지 파일 이름에 "/img/ 경로를 붙여서 board 객체에 저장"
+			b.setImg("/img/" + fileName);
+			
+			dao.insertBoard(b);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		/*
+		b.setUser_id(request.getParameter("user_id"));
+		*/
+		
+		return "redirect:/list";
+	}
+	
+	//게시글 수정 보여주는 메소드
+	public String getViewForEdit(HttpServletRequest request) {
+		int board_no = Integer.parseInt(request.getParameter("board_no"));
+		
+		try {
+			Board b = dao.getViewForEdit(board_no);
+			request.setAttribute("board", b);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return "edit.jsp";
+	}
+	
+	//게시글 수정 업데이트 메소드
+	public String updateBoard(HttpServletRequest request) {
+		Board b = new Board();
+		try {
+			BeanUtils.populate(b, request.getParameterMap());
+			
+			//1. 이미지 파일 자체를 서버 컴퓨터에 저장
+			Part part = request.getPart("file"); //이미지파일 받기
+			String fileName = getFilename(part); //파일 이름 구하기
+			
+			if(fileName != null && !fileName.isEmpty()) { //fileName이 null이 아니고 빈값이 아니라면
+				part.write(fileName); //파일을 컴퓨터에 저장한다.
+			}
+			
+			//2. 이미지 파일 이름에 "/img/ 경로를 붙여서 board 객체에 저장"
+			b.setImg("/img/" + fileName);
+			
+			dao.updateBoard(b);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return "redirect:/view?board_no=" + b.getBoard_no();
+	}
+	
+	//글 삭제
+	public String deleteBoard(HttpServletRequest request) {
+		int board_no = Integer.parseInt(request.getParameter("board_no"));
+		
+		try {
+			dao.deleteBoard(board_no);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "redirect:/list";
+		
+	}
+	
+	
+	//파일 이름 추출
+	private String getFilename(Part part) {
+		String fileName = null;
+		String header = part.getHeader("content-disposition");
+		System.out.println("header => " + header);
+		
+		
+		int start = header.indexOf("filename=");
+		fileName = header.substring(start + 10, header.length() -1);
+		System.out.println("파일명: " + fileName);
+		
+		return fileName;
+	}
+	
+	
+	
 }
